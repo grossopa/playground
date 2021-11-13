a1af38c573d4ee3301fb2645ed920e57cfcea571
Jack Yin <grossopaforever@gmail.com>
2021-11-13 22:59:15 +0800

    WIP set up consul demo projects

---
+++ consul/consul-apps/consul-apps-user/src/main/java/com/github/grossopa/consul/user/AppConfig.java
@@ -0,0 +1,58 @@
+package com.github.grossopa.consul.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * The main configuration
+ *
+ * @author Jack Yin
+ * @since 1.0
+ */
+@EnableOpenApi
+@EnableSwagger2
+@EnableWebSecurity
+@EnableAutoConfiguration
+@EnableScheduling
+@ComponentScan
+@Configuration
+@Slf4j
+public class AppConfig {
+
+
+    @Configuration
+    public static class PermitAllConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll();
+        }
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any()).build();
+    }
+}
---
+++ consul/consul-apps/consul-apps-user/src/main/java/com/github/grossopa/consul/user/controller/UserController.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the The MIT License (MIT) (the "License");
+ *  You may obtain a copy of the License at
+ *
+ *         https://mit-license.org/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.grossopa.consul.user.controller;
+
+import com.github.grossopa.consul.user.model.UserLoginDto;
+import com.github.grossopa.consul.user.model.UserLoginRequestDto;
+import com.github.grossopa.consul.user.service.UserLoginService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Jack Yin
+ * @since 1.0
+ */
+@Api(tags = "user")
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserLoginService userLoginService;
+
+    @ApiOperation("login")
+    @PostMapping("/login")
+    public UserLoginDto login(@RequestBody UserLoginRequestDto requestDto) {
+        log.info("User login request {}", requestDto.getUid());
+        log.debug("Request token {}", requestDto.getRequestToken());
+
+        return userLoginService.login(requestDto.getUid(), requestDto.getRequestToken());
+    }
+}
